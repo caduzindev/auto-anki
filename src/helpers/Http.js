869f0f4d9b03952003b2d1fce03dfde7b72bc769
Carlos Eduardo Almeida Silva <cadudevmaster@gmail.com>
2022-07-03 17:18:04 -0300

    feat: adicionado metodo post para Http helper

---
+++ src/helpers/Http.js
@@ -0,0 +1,42 @@
+import { request } from 'http'
+
+class Http
+{
+    static _config = {
+        hostname: 'http://localhost',
+        port: 80,
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }
+
+    static async post(obj)
+    {
+
+        return new Promise((resolve,reject)=>{
+            const requiredFields = ['data','path','method']
+
+            for (const field of requiredFields)
+            {
+                if (!obj[field]) throw new Error(`Campo ${field} não informado`)
+            }
+
+            const data = JSON.stringify(obj.data)
+            const req = request({
+                ...this._config,
+                ...obj
+            },res => {
+                let body = ''
+
+                res.on('data',data=> body += data)
+                res.on('end',() => resolve(body))
+            })
+            req.on('error',(e) => reject(e.message))
+
+            req.write(data)
+            req.end()
+        })
+    }
+}
+
+export default Http
---
+++ tests/unit/helpers/Http.spec.js
@@ -0,0 +1,29 @@
+import Http from '../../../src/helpers/Http'
+import http from 'http'
+
+describe('Test Http', ()=>{
+    test('should throw error if missing data (request post)',async ()=>{
+       const data = {
+        path: '/fake',
+        method: 'POST',
+       }
+
+       expect(()=>Http.post(data)).rejects.toThrowError(new Error('Campo data não informado'))
+    })
+    test('should throw error if missing path (request post)',async ()=>{
+       const data = {
+        data: {},
+        method: 'POST',
+       }
+
+       expect(()=>Http.post(data)).rejects.toThrowError(new Error('Campo path não informado'))
+    })
+    test('should throw error if missing method (request post)',async ()=>{
+      const data = {
+        path: '/fake',
+        data: {},
+       }
+
+       expect(()=>Http.post(data)).rejects.toThrowError(new Error('Campo method não informado'))
+    })
+})
